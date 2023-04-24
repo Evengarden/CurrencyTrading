0fa9fcef804754e5021f18f04e6013340aa97586
Danil Sitnikov <60429893+Evengarden@users.noreply.github.com>
2023-04-24 21:43:48 +0700

    test: UserService
    Add tests for UserService, add templates for tests other services

---
+++ CurrencyTrading.test/src/ServicesTests/MatchingServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrading.test.src.ServicesTests
+{
+    public class MatchingServiceTests
+    {
+    }
+}
