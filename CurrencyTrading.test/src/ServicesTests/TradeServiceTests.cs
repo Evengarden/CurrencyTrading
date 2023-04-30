d244e96d7f0776b460560ebe523a6fc8d9328a91
Evengarden <dan20012001@bk.ru>
2023-04-30 14:07:19 +0700

    tests: Add service tests
    Add tests for TradeService

--- CurrencyTrading.test/src/ServicesTests/TradeServiceTests.cs
+++ CurrencyTrading.test/src/ServicesTests/TradeServiceTests.cs
@@ -1,7 +1,9 @@
-﻿using CurrencyTrading.Data;
+﻿using CurrencyTrading.DAL.DTO;
+using CurrencyTrading.Data;
 using CurrencyTrading.Helper;
 using CurrencyTrading.Interfaces;
 using CurrencyTrading.Models;
+using CurrencyTrading.services.CustomExceptions;
 using CurrencyTrading.services.Services;
 using Microsoft.EntityFrameworkCore;
 using Moq;
@@ -43,13 +45,38 @@ namespace CurrencyTrading.test.src.ServicesTests
                 Login = "test1",
                 Password = HashPassword.HashPass("test1")
             }).Entity;
+            _user.Balance = new List<Balance> {
+                new Balance {
+                Amount = 100,
+                Currency ="USD",
+                User = _user
+                },
+                new Balance {
+                Amount = 10000,
+                Currency ="RUB",
+                User = _user
+                }
+            };
+            _user2.Balance = new List<Balance> {
+                new Balance {
+                Amount = 100,
+                Currency ="USD",
+                User = _user2
+                },
+                new Balance {
+                Amount = 10000,
+                Currency ="RUB",
+                User = _user2
+                }
+            };
             _lot = _ctx.Lots.Add(new Lot
             {
                 Currency = "USD",
                 CurrencyAmount = 10,
-                Status = 0,
+                Status = Statuses.Created,
                 Price = 100,
-                OwnerId = _user.Id
+                OwnerId = _user.Id,
+                Type = Types.Sold
             }).Entity;
             _tradeRepository = new Mock<ITradeRepository>();
             _lotRepository = new Mock<ILotRepository>();
@@ -58,21 +85,155 @@ namespace CurrencyTrading.test.src.ServicesTests
         }
 
         [Fact]
-        public async Task CreateTrade_ShouldReturnCreatedTrade()
+        public async Task CreateTrade_ShouldReturnCreatedTradeForSoldLot()
         {
+            //arrange
+            _userRepository.Setup(u => u.GetUserAsync(_user.Id)).ReturnsAsync(_user);
+            _userRepository.Setup(u => u.GetUserAsync(_user2.Id)).ReturnsAsync(_user2);
+            _lotRepository.Setup(l=>l.GetLotAsync(_lot.Id)).ReturnsAsync(_lot);
+            TradeDTO tradeDTO = new TradeDTO
+            {
+                LotId=_lot.Id
+            };
+            Trade trade = new Trade
+            {
+                Buyer = _user2,
+                TradeLot=_lot,
+                LotId = tradeDTO.LotId,
+                BuyerId = _user2.Id,
+                TradeDate = DateTime.Now,
+            };
+            _tradeRepository.Setup(t => t.CreateTradeAsync(It.IsAny<Trade>())).ReturnsAsync(trade);
+            _lotRepository.Setup(l => l.UpdateLotAsync(_lot.Id,_lot)).ReturnsAsync(_lot);
+            //act
+            var result = await _tradeService.CreateTrade(tradeDTO, _user2.Id);
+            //assert
+            Assert.NotNull(result);
+        }
 
+        [Fact]
+        public async Task CreateTrade_ShouldReturnCreatedTradeForBuyLot()
+        {
+            //arrange
+            _lot.Type = Types.Buy;
+            _userRepository.Setup(u => u.GetUserAsync(_user.Id)).ReturnsAsync(_user);
+            _userRepository.Setup(u => u.GetUserAsync(_user2.Id)).ReturnsAsync(_user2);
+            _lotRepository.Setup(l => l.GetLotAsync(_lot.Id)).ReturnsAsync(_lot);
+            TradeDTO tradeDTO = new TradeDTO
+            {
+                LotId = _lot.Id
+            };
+            Trade trade = new Trade
+            {
+                Buyer = _user2,
+                TradeLot = _lot,
+                LotId = tradeDTO.LotId,
+                BuyerId = _user2.Id,
+                TradeDate = DateTime.Now,
+            };
+            _tradeRepository.Setup(t => t.CreateTradeAsync(It.IsAny<Trade>())).ReturnsAsync(trade);
+            _lotRepository.Setup(l => l.UpdateLotAsync(_lot.Id, _lot)).ReturnsAsync(_lot);
+            //act
+            var result = await _tradeService.CreateTrade(tradeDTO, _user2.Id);
+            //assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public async Task CreateTrade_ShouldReturnCustomErrorBalanceNotEnoughForSoldLot()
+        {
+            //arrange
+            var balance = _user.Balance.Where(b=>b.Currency=="USD").FirstOrDefault();
+            balance.Amount = 0;
+            _userRepository.Setup(u => u.GetUserAsync(_user.Id)).ReturnsAsync(_user);
+            _userRepository.Setup(u => u.GetUserAsync(_user2.Id)).ReturnsAsync(_user2);
+            _lotRepository.Setup(l => l.GetLotAsync(_lot.Id)).ReturnsAsync(_lot);
+            TradeDTO tradeDTO = new TradeDTO
+            {
+                LotId = _lot.Id
+            };
+            Trade trade = new Trade
+            {
+                Buyer = _user2,
+                TradeLot = _lot,
+                LotId = tradeDTO.LotId,
+                BuyerId = _user2.Id,
+                TradeDate = DateTime.Now,
+            };
+            _tradeRepository.Setup(t => t.CreateTradeAsync(It.IsAny<Trade>())).ReturnsAsync(trade);
+            _lotRepository.Setup(l => l.UpdateLotAsync(_lot.Id, _lot)).ReturnsAsync(_lot);
+            //act
+            Assert.ThrowsAsync<NotEnoughBalanceForSold>(async()=>await _tradeService.CreateTrade(tradeDTO, _user2.Id));
+        }
+
+        [Fact]
+        public async Task CreateTrade_ShouldReturnCustomErrorBalanceNotEnoughForBuyLot()
+        {
+            //arrange
+            _lot.Type = Types.Buy;
+            var balance = _user.Balance.Where(b => b.Currency == "RUB").FirstOrDefault();
+            balance.Amount = 0;
+            _userRepository.Setup(u => u.GetUserAsync(_user.Id)).ReturnsAsync(_user);
+            _userRepository.Setup(u => u.GetUserAsync(_user2.Id)).ReturnsAsync(_user2);
+            _lotRepository.Setup(l => l.GetLotAsync(_lot.Id)).ReturnsAsync(_lot);
+            TradeDTO tradeDTO = new TradeDTO
+            {
+                LotId = _lot.Id
+            };
+            Trade trade = new Trade
+            {
+                Buyer = _user2,
+                TradeLot = _lot,
+                LotId = tradeDTO.LotId,
+                BuyerId = _user2.Id,
+                TradeDate = DateTime.Now,
+            };
+            _tradeRepository.Setup(t => t.CreateTradeAsync(It.IsAny<Trade>())).ReturnsAsync(trade);
+            _lotRepository.Setup(l => l.UpdateLotAsync(_lot.Id, _lot)).ReturnsAsync(_lot);
+            //act
+            Assert.ThrowsAsync<NotEnoughBalanceForBuy>(async () => await _tradeService.CreateTrade(tradeDTO, _user2.Id));
         }
 
         [Fact]
         public async Task GetTrade_ShouldReturnTrade()
         {
-
+            //arrange
+            Trade trade = new Trade
+            {
+                Id = 1,
+                Buyer = _user2,
+                TradeLot = _lot,
+                LotId = _lot.Id,
+                BuyerId = _user2.Id,
+                TradeDate = DateTime.Now,
+            };
+            _tradeRepository.Setup(t=>t.GetTradeAsync(It.IsAny<int>())).ReturnsAsync(trade);
+            //act
+            var result = await _tradeService.GetTrade(trade.Id);
+            //assert
+            Assert.NotNull(result);
         }
 
         [Fact]
         public async Task GetTrades_ShouldReturnTrades()
         {
-
+            //arrange
+            List<Trade> trades = new List<Trade>{ new Trade
+                {
+                    Id = 1,
+                    Buyer = _user2,
+                    TradeLot = _lot,
+                    LotId = _lot.Id,
+                    BuyerId = _user2.Id,
+                    TradeDate = DateTime.Now,
+                }
+            };
+            _tradeRepository.Setup(t => t.GetTradesAsync()).ReturnsAsync(trades);
+            //act
+            var result = await _tradeService.GetTrades();
+            //assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
         }
     }
 }
