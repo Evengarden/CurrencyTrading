49e58151731e5186e1c259636b3a66c752e36060
Danil Sitnikov <60429893+Evengarden@users.noreply.github.com>
2023-05-05 14:15:07 +0700

    refactor: Create startup.cs
    Move DI and configure app logic into file startup.cs

---
+++ CurrencyTrading.src/Startup.cs
@@ -0,0 +1,116 @@
+﻿using CurrencyTrading.Data;
+using Quartz;
+using CurrencyTrading.Interfaces;
+using CurrencyTrading.Repository;
+using CurrencyTrading.services.Helpers;
+using CurrencyTrading.services.Interfaces;
+using CurrencyTrading.services.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace CurrencyTrading
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration =  configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("PostgresConnection"));
+            });
+
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<ILotRepository, LotRepository>();
+            services.AddTransient<ITradeRepository, TradeRepository>();
+            services.AddTransient<IBalanceRepository, BalanceRepository>();
+
+            services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IBalanceService, BalanceService>();
+            services.AddTransient<ILotService, LotService>();
+            services.AddTransient<ITradeService, TradeService>();
+            services.AddTransient<IIntegrationService, IntegrationService>();
+
+            services.AddStackExchangeRedisCache(options => {
+                options.Configuration = "redis:6379,abortConnect=false";
+            });
+
+            services.AddQuartz(q =>
+            {
+                q.UseMicrosoftDependencyInjectionScopedJobFactory();
+                var getCurrencyJobKey = new JobKey("GetCurrencyFromCb");
+                q.AddJob<IntegrationService>(opts => opts.WithIdentity(getCurrencyJobKey));
+
+                q.AddTrigger(opts => opts
+                    .ForJob(getCurrencyJobKey)
+                    .WithIdentity("GetCurrencyFromCb-trigger")
+                    .WithCronSchedule("0 0 0 * * ?")
+                );
+
+                var startAutomatchingJobKey = new JobKey("Automatching");
+                q.AddJob<MatchingService>(opts => opts.WithIdentity(startAutomatchingJobKey));
+
+                q.AddTrigger(opts => opts
+                    .ForJob(startAutomatchingJobKey)
+                    .WithIdentity("Automatching-trigger")
+                    .WithCronSchedule("0 */10 * ? * *")
+                );
+            });
+            services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+            services.Configure<JWTSettings>(Configuration.GetSection("JWTSettings"));
+            var secretKey = Configuration.GetSection("JWTSettings:SecretKey").Value;
+            var issuer = Configuration.GetSection("JWTSettings:Issuer").Value;
+            var audience = Configuration.GetSection("JWTSettings:Audience").Value;
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = false,
+                    IssuerSigningKey = signingKey,
+                    ValidateIssuerSigningKey = true
+                };
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                var context = services.GetRequiredService<DataContext>();
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+            }
+
+            app.Run();
+        }
+    }
+}
